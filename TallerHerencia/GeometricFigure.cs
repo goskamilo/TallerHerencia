e33025e47eb0cb45ac6a10d6ef99559262eb6cef
familiar <goskamilo@outlook.com>
2026-08-30 18:49:37 -0500

    comming

--- TallerHerencia/GeometricFigure.cs
+++ TallerHerencia/GeometricFigure.cs
@@ -1,10 +1,26 @@
 ﻿
+using System.Dynamic;
+
 namespace Backend
 {
     public abstract class GeometricFigure
     {
 
-
+        // Propierties
+       
+        // Methods
+        public string Name
+        {
+            get;
+            set;
+        }
+        // Methods
+        public abstract double GetArea();
+        public abstract double GetPerimiter();
+        public override string ToString()
+        {
+            return $"{Name}- Area: {GetArea():D4}/Perimeter: {GetPerimiter():D4}";
+        }
 
 
     }
