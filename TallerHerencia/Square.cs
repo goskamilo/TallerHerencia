b8f643dc0ba065e0f5b57eccebd04ce2c39e2282
familiar <goskamilo@outlook.com>
2026-08-30 20:11:44 -0500

    Comming profress square

--- TallerHerencia/Square.cs
+++ TallerHerencia/Square.cs
@@ -1,7 +1,41 @@
-﻿namespace TallerHerencia
+﻿using Backend;
+
+namespace TallerHerencia
 {
-    public class Square
+    public class Square : GeometricFigure
     {
 
+        //Fields
+        private double _a;
+        //Constructor
+        public Square(double A)
+        {
+           _a= A;
+        }
+
+        //Propierty
+        public double A
+        {
+            get  => _a;
+            set => _a=ValidateA(value);
+        }    
+        // Methods
+        public override double GetArea()
+        {
+            return A * A;
+        }
+        public override double GetPerimiter()
+        {
+            return 4 * A;
+        }
+        // Private Method
+        private double ValidateA(double value)
+        {
+            if (value <= 0)
+            {
+                Console.WriteLine($"{value}/Error: the value must be greater than zero");
+            }
+            return value;
+        }
     }
 }
